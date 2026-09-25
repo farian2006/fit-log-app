d63a36a815dbb676b3ae4402fbd27a1130291ad0
farian2006 <fariantazwar@gmail.com>
2026-09-25 12:20:26 +0600

    Footer Added

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Image from 'next/image'
 
 const Footer = () => {
     return (
-     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-  <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
-  </aside>
+     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 container mx-auto">
+  <div className='flex justify-between gap-74'>
+    <div className='flex gap-4 items-center'>
+   <Image src='/assests/logo.png' width={50} height={50} alt='Logo Image'></Image>
+   <p className='font-bold'>FITLOG</p>
+   </div>
+  <p className='text-gray-400'>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
+  </div>
 </footer>
     );
 };
